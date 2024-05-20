6cc8e694e8ae491c1fd69032c8636ff4836beccc
Tronikel <contactdonatas@gmail.com>
2024-05-20 15:54:44 +0300

    branch oil should work now

--- lua/lualine-components/branch-oil/apply_patch.sh
+++ lua/lualine-components/branch-oil/apply_patch.sh
@@ -1,3 +1,5 @@
+rm *.lua
+
 wget "https://raw.githubusercontent.com/nvim-lualine/lualine.nvim/master/lua/lualine/components/branch/init.lua"
 wget "https://raw.githubusercontent.com/nvim-lualine/lualine.nvim/master/lua/lualine/components/branch/git_branch.lua"
 
--- lua/lualine-components/filename-oil/apply_patch.sh
+++ lua/lualine-components/filename-oil/apply_patch.sh
@@ -1,3 +1,5 @@
+rm *.lua
+
 url="https://raw.githubusercontent.com/nvim-lualine/lualine.nvim/master/lua/lualine/components/filename.lua"
 
 wget -O "./init.lua" "$url"
