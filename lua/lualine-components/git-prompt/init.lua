19a6b3db778586866dcdd2767b81def26c7f44a7
Tronikel <contactdonatas@gmail.com>
2024-10-10 12:24:05 +0300

    git prompt only run when neeeded

--- lua/lualine-components/git-prompt/init.lua
+++ lua/lualine-components/git-prompt/init.lua
@@ -1,10 +1,45 @@
 local M = require("lualine.component"):extend()
 
--- making these global, because git runs on cwd anyway, so all buffers will be the same
-local prompt = ""
-local running = false
+function M:parse_git_status()
+	if self.running then
+		return
+	end
+	self.running = true
+
+	vim.system(
+		{ "git", "status", "--porcelain" },
+		{},
+		vim.schedule_wrap(function(out)
+			self.running = false
+
+			if not out.stdout then
+				return
+			end
+
+			local statuses = {}
+
+			local lines = vim.split(out.stdout, "\n", { trimempty = true })
+			for _, line in ipairs(lines) do
+				line = vim.trim(line)
+				line = vim.split(line, " ", { trimempty = true })[1]
+
+				for _, status in ipairs(vim.split(line, "", { trimempty = true })) do
+					table.insert(statuses, status)
+				end
+			end
+
+			statuses = vim.fn.uniq(statuses)
+			table.sort(statuses)
+
+			self.prompt = vim.fn.join(statuses, "")
+		end)
+	)
+end
 
 function M:init(options)
+	self.prompt = ""
+	self.running = false
+
 	if not options.color then
 		local fg = vim.api.nvim_get_hl(0, { name = "Error" }).fg
 		options.color = {
@@ -12,45 +47,18 @@ function M:init(options)
 		}
 	end
 
+	self:parse_git_status()
+	vim.api.nvim_create_autocmd({ "FocusGained", "BufWritePost" }, {
+		callback = function()
+			self:parse_git_status()
+		end,
+	})
+
 	M.super.init(self, options)
 end
 
 function M:update_status()
-	if running then
-		return prompt
-	end
-	running = true
-
-	local function parse(git_status)
-		if not git_status then
-			return
-		end
-
-		local statuses = {}
-
-		local lines = vim.split(git_status, "\n", { trimempty = true })
-		for _, line in ipairs(lines) do
-			line = vim.trim(line)
-			line = vim.split(line, " ", { trimempty = true })[1]
-
-			for _, status in ipairs(vim.split(line, "", { trimempty = true })) do
-				table.insert(statuses, status)
-			end
-		end
-
-		statuses = vim.fn.uniq(statuses)
-		table.sort(statuses)
-
-		prompt = vim.fn.join(statuses, "")
-	end
-	parse = vim.schedule_wrap(parse)
-
-	vim.system({ "git", "status", "--porcelain" }, {}, function(obj)
-		parse(obj.stdout)
-		running = false
-	end)
-
-	return prompt
+	return self.prompt
 end
 
 return M
